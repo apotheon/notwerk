5a398eb708d58690b65b43f25f88e3fed245c8f4
ren <ren@noemail.net>
2019-08-24 00:58:04 +0000

    limit string dup to PATH_MAX
    FossilOrigin-Name: b81a5830dc5100b2497a532b25f8abddc0b86cb0

--- src/nw.c
+++ src/nw.c
@@ -1,10 +1,11 @@
 #include <libgen.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char **argv) {
 	const char *program = *(argv);
-	printf("USAGE: %s\n", basename(strdup(program)));
+	printf("USAGE: %s\n", basename(strndup(program, PATH_MAX)));
 
 	return 0;
 }
