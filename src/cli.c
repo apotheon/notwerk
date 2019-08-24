4144170d09406733c51e5b8a68e7f3aba8a146bb
ren <ren@noemail.net>
2019-08-24 01:45:44 +0000

    use const char param for usage_text()
    FossilOrigin-Name: 9c1e0cf5ac83f2a72544d3f8761244d2b38fe1a1

--- src/cli.c
+++ src/cli.c
@@ -1,6 +1,6 @@
 #include "cli.h"
 
-char *usage_text(char *self) {
+char *usage_text(const char *self) {
 	char *text = calloc(LINESIZE, sizeof(*text));
 	strlcpy(text, "USAGE: ", LINESIZE);
 	strlcat(text, self, LINESIZE);
