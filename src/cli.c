ed9ef30deff6a071527c678eff736974d5c01bcc
ren <code@apotheon.net>
2019-08-25 04:55:01 +0000

    add <COMMAND> to usage text

--- src/cli.c
+++ src/cli.c
@@ -13,6 +13,7 @@ char *usage_text(const char *self) {
 	char *text = calloc(LINESIZE, sizeof(*text));
 	strlcpy(text, "USAGE: ", LINESIZE);
 	strlcat(text, self, LINESIZE);
+	strlcat(text, " <COMMAND>", LINESIZE);
 
 	return text;
 }
