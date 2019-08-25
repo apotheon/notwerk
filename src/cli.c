794bfff85fd8079868dd9ba4eda9922d945c5daa
ren <ren@noemail.net>
2019-08-25 05:18:06 +0000

    add help_text() to CLI
    FossilOrigin-Name: 7f1e1ef4de5ed8f6b9726ccc45f99fe900cad872

--- src/cli.c
+++ src/cli.c
@@ -1,5 +1,16 @@
 #include "cli.h"
 
+char *help_text() {
+	char *text = (
+		"\n"
+		"COMMANDS:\n"
+		"\n"
+		"    h[elp]                  Display this help message.\n"
+	);
+
+	return text;
+}
+
 char *try_text(const char *self) {
 	char *text = calloc(LINESIZE, sizeof(*text));
 	strlcpy(text, "Try \"", LINESIZE);
