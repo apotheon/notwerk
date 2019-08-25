24f3fbefc0c2d5e2d9046c402efe71af4a94c140
ren <ren@noemail.net>
2019-08-25 21:54:32 +0000

    implement strlist_size() in util.c
    FossilOrigin-Name: da8bbe0033d28d25f93d3ed9788a337219d02704

---
+++ src/util.c
@@ -0,0 +1,13 @@
+#include "util.h"
+
+size_t strlist_size(uint64_t n, char **stringlist) {
+	size_t total = 0;
+
+	for (uint64_t i = 0;  i < n; ++i) {
+		size_t typesize = sizeof(*(stringlist + 1));
+		size_t stringlength = (strlen(*(stringlist + 1)) + 1);
+		total += typesize * stringlength;
+	}
+
+	return total;
+}
