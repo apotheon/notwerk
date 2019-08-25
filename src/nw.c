284ed0fdebfc2b68c99d6e3b79de7ad08026203a
ren <code@apotheon.net>
2019-08-25 05:57:17 +0000

    handle help command in nw

--- src/nw.c
+++ src/nw.c
@@ -2,8 +2,19 @@
 
 int main(int argc, char **argv) {
 	const char *program = *(argv);
-	puts(usage_text(program));
-	puts(try_text(program));
+
+	if (argc == 1) {
+		puts(usage_text(program));
+		puts(try_text(program));
+	} else if (argc == 2) {
+		char *cmd = *(argv + 1);
+
+		if (match_cmd(cmd, "help")) {
+			puts("");
+			puts(usage_text(program));
+			puts(help_text());
+		}
+	}
 
 	return 0;
 }
