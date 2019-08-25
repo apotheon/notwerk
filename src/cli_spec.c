9bdf7cbf7e1d2d6629e89eb80bab565739fc7ecd
ren <code@apotheon.net>
2019-08-25 04:56:51 +0000

    reorder function specs in cli spec

--- src/cli_spec.c
+++ src/cli_spec.c
@@ -2,17 +2,17 @@
 #include "cli.h"
 
 spec("cli") {
-	describe("usage_text()") {
-		it("should return usage text") {
-			check(strncmp(usage_text("nw"), "USAGE: nw", LINESIZE) == 0);
-		}
-	}
-
 	describe("try_text()") {
 		it("should return try text") {
 			char *try_text_string = "Try \"nw help\" for more info.";
 			puts(try_text("nw"));
 			check(strncmp(try_text("nw"), try_text_string, LINESIZE) == 0);
+		}
+	}
+
+	describe("usage_text()") {
+		it("should return usage text") {
+			check(strncmp(usage_text("nw"), "USAGE: nw", LINESIZE) == 0);
 		}
 	}
 }
