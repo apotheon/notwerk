5abcb6951ecfe07f2c969f3ce3f7611d15a05842
ren <ren@noemail.net>
2019-08-25 05:17:09 +0000

    cli spec: add help_text() spec; update usage_text() spec
    FossilOrigin-Name: 128bcb11f610de4fc1fb962b18ce63242757fc45

--- src/cli_spec.c
+++ src/cli_spec.c
@@ -1,18 +1,24 @@
 #include "bdd-for-c.h"
 #include "cli.h"
 
-spec("cli") {
+spec("CLI") {
+	describe("help_text()") {
+		it("should return help text") {
+			check(strstr(help_text(), "Display this help message."));
+		}
+	}
+
 	describe("try_text()") {
 		it("should return try text") {
 			char *try_text_string = "Try \"nw help\" for more info.";
-			puts(try_text("nw"));
 			check(strncmp(try_text("nw"), try_text_string, LINESIZE) == 0);
 		}
 	}
 
 	describe("usage_text()") {
 		it("should return usage text") {
-			check(strncmp(usage_text("nw"), "USAGE: nw", LINESIZE) == 0);
+			char *usage_text_string = "USAGE: nw <COMMAND>";
+			check(strncmp(usage_text("nw"), usage_text_string, LINESIZE) == 0);
 		}
 	}
 }
