ba5f72b4836e8f67e2c594847b6c3a4a4a366127
ren <code@apotheon.net>
2019-08-25 05:44:46 +0000

    add match_cmd() to CLI

--- src/cli.h
+++ src/cli.h
@@ -6,10 +6,13 @@
 #include <libgen.h>
 #include <limits.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 #endif
+
+bool match_cmd(char *cmd, char *cmdtarget);
 
 char *help_text();
 char *try_text(const char *self);
