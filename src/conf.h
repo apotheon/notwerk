cb921dba882401687f9b29279885d9ea42961fdd
ren <code@apotheon.net>
2019-09-02 06:20:15 +0000

    add conf.h skeleton

---
+++ src/conf.h
@@ -0,0 +1,7 @@
+struct conf_struct {
+	char name = calloc(/* length */, /* typesize */);
+	/* other lines of meaningful config data */
+	/* maybe function pointers for "object methods" */
+} conf;
+
+/* struct conf **net_list = <something goes here>; */
